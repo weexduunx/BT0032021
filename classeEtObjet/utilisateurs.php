b18e3ef850636f8cd440a8e992b5e73468865c28
Idrissa Ndiouck <wduunx7@gmail.com>
2021-12-24 13:14:16 +0000

    //La méthode de construction de la base de donnée

--- classeEtObjet/utilisateurs.php
+++ classeEtObjet/utilisateurs.php
@@ -4,8 +4,14 @@ include ('db/connexion.php');
 //On crée la classe des utilisateurs
 
 class Utilisateurs{
+    //Propriété de la base de donnée (attribut private afin de définir la propriété bdd en mode privée)
+    private $bdd;
 
-    
+    //La méthode de construction de la base de donnée
+    public function __construct(){
+       $this->bdd = new BaseDeDonnee(); 
+    }
+
 }
 
 
